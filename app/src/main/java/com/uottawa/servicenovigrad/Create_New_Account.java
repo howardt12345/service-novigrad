25b079f4c490c0fae704e12a1b929bd8351e511f
faith176 <feyi.adesanya176@gmail.com>
2020-09-22 17:32:38 -0600

    Create New Account activity created and successful sign in modified

---
+++ app/src/main/java/com/uottawa/servicenovigrad/Create_New_Account.java
@@ -0,0 +1,14 @@
+package com.uottawa.servicenovigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_New_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__new__account);
+    }
+}
