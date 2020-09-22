d595332e7f08d790ddfbebacbf670fdb5d120fac
faith176 <feyi.adesanya176@gmail.com>
2020-09-21 19:26:22 -0600

    Login and Successful Login Activities

--- app/src/main/java/com/uottawa/servicenovigrad/MainActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/MainActivity.java
@@ -3,6 +3,10 @@ package com.uottawa.servicenovigrad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login_button  = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //stores the editText from login page in variables
+                EditText login_usernameEntry = (EditText) findViewById(R.id.login_usernameEntry);
+                EditText login_passwordEntry = (EditText) findViewById(R.id.login_passwordEntry);
+                //Get values of username and password variables
+                String username = (login_usernameEntry.getText().toString());
+                String password = (login_passwordEntry.getText().toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/uottawa/servicenovigrad/Successful_login.java
@@ -0,0 +1,14 @@
+package com.uottawa.servicenovigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Successful_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_login);
+    }
+}
