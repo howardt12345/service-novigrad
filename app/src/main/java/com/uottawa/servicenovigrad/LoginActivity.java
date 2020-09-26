276cd1783694eda5f07070aa6a584c7a65b7fe06
Howard Tseng <howardt12345@users.noreply.github.com>
2020-09-26 15:52:52 -0400

    Added error showing on login
    Error shows as a snackbar. when dismissed, the email and password fields clear

--- app/src/main/java/com/uottawa/servicenovigrad/LoginActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/LoginActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -21,17 +24,45 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //stores the editText from login page in variables
-                EditText login_emailEntry = (EditText) findViewById(R.id.login_emailEntry);
-                EditText login_passwordEntry = (EditText) findViewById(R.id.login_passwordEntry);
+                final EditText login_emailEntry = (EditText) findViewById(R.id.login_emailEntry);
+                final EditText login_passwordEntry = (EditText) findViewById(R.id.login_passwordEntry);
                 //Get values of username and password variables
                 String username = (login_emailEntry.getText().toString());
                 String password = (login_passwordEntry.getText().toString());
 
+                String errorMessage = "";
+
                 //TODO: Check if input is valid, then sign into Firebase
 
-                //Navigate to Main Activity if login is valid
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if(username.compareTo("admin") == 0) {
+                    errorMessage = "no";
+                }
+
+                //If there is an error
+                if(!errorMessage.isEmpty()) {
+                    //Show a snackbar with the error message
+                    Snackbar mySnackbar = Snackbar.make(findViewById(R.id.login_page), errorMessage, BaseTransientBottomBar.LENGTH_SHORT);
+                    //Add close button
+                    mySnackbar.setAction("CLOSE", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    //Clear text when snackbar is closed
+                    mySnackbar.addCallback(new Snackbar.Callback() {
+                        @Override
+                        public void onDismissed(Snackbar snackbar, int event) {
+                            login_emailEntry.getText().clear();
+                            login_passwordEntry.getText().clear();
+                        }
+                    });
+                    //Show snackbar
+                    mySnackbar.show();
+                } else {
+                    //Navigate to Main Activity if login is valid
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
