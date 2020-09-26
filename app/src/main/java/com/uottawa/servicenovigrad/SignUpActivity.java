8728df41cdf79eff7e97bc9fe4de03ac41399dee
Howard Tseng <howardt12345@users.noreply.github.com>
2020-09-26 16:06:56 -0400

    Update SignUpActivity.java
    This reverts commit 989a74e710205ed6e2b52a89e04be7589b422ea3.

--- app/src/main/java/com/uottawa/servicenovigrad/SignUpActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.uottawa.servicenovigrad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getSupportActionBar().hide();
+
+        Button signUp_login_button = (Button) findViewById(R.id.signUp_login);
+        signUp_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Navigate to Login Activity
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
