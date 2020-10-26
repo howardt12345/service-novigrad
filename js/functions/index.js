05c5a55e007b5f95d5d7beaa911b16b4cbcbdd67
Howard Tseng <howardt12345@users.noreply.github.com>
2020-10-26 16:49:36 -0400

    Deletes user when document with user uid is deleted

--- js/functions/index.js
+++ js/functions/index.js
@@ -7,3 +7,14 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+//Deletes the user when the firestore document with the given user uid is deleted
+export const removeUser = functions.firestore.document("/users/{uid}")
+    .onDelete((snapshot, context) => {        
+        const serviceAccount = require('path/to/serviceAccountKey.json');
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://<DATABASE_NAME>>.firebaseio.com"
+        });
+        return admin.auth().deleteUser(context.params.uid);
+    });
