d812ec8d72381a1dec4f4024152e63191bb9243d
Howard Tseng <howardt12345@users.noreply.github.com>
2020-10-26 20:35:08 -0400

    Added admin activities

---
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/admin/AdminUsersActivity.java
@@ -0,0 +1,15 @@
+package com.uottawa.servicenovigrad.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.uottawa.servicenovigrad.R;
+
+public class AdminUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_users);
+    }
+}
--- app/src/main/java/com/uottawa/servicenovigrad/user/AdminAccount.java
+++ app/src/main/java/com/uottawa/servicenovigrad/user/AdminAccount.java
@@ -6,4 +6,12 @@ public class AdminAccount extends UserAccount {
     public AdminAccount(String uid) {
         super("admin", "admin", "admin", uid);
     }
+
+    public UserAccount[] getEmployees() {
+        return null;
+    }
+
+    public UserAccount[] getCustomers() {
+        return null;
+    }
 }
