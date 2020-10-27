b8cac8e2f2d36d298facc6b7302976f45281bd23
Howard Tseng <howardt12345@users.noreply.github.com>
2020-10-26 20:34:36 -0400

    Removed temp main activity code

--- app/src/main/java/com/uottawa/servicenovigrad/activities/MainActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/MainActivity.java
@@ -20,15 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        TextView title = (TextView) findViewById(R.id.successful_login);
-        TextView info = (TextView) findViewById(R.id.login_info);
-
         UserAccount account = UserController.getInstance().getUserAccount();
-
-        //Write the name, role, and email of the current user.
-        title.setText("Welcome " + account.getName() + "!");
-        info.setText("You are logged in as a " + account.getRole() + "\nYour email is: " + account.getEmail());
-
 
         Intent intent;
         //Sets intent based on what role the user is
@@ -46,20 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(MainActivity.this, LoginActivity.class);
                 break;
         }
-        //set the new task and clear flags, so that the user can't go back here
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    /**
-     * Sign out of the app.
-     * @param view The current view.
-     */
-    public void signOut(View view) {
-        //Sign out of Firebase
-        UserController.getInstance().signOut();
-        //Navigate back to Login Page
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         //set the new task and clear flags, so that the user can't go back here
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
