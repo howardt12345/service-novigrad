0cf278abaa4e4003a9797fa3bb12107ba760015a
Howard Tseng <howardt12345@users.noreply.github.com>
2020-11-08 19:29:16 -0500

    Branch object

---
+++ app/src/main/java/com/uottawa/servicenovigrad/branch/Branch.java
@@ -0,0 +1,8 @@
+package com.uottawa.servicenovigrad.branch;
+
+public class Branch {
+    private String name;
+    private String description;
+    private String address;
+    private String phoneNumber;
+}
