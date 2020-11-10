2e6921139873a4856e3820f971cb3e5eb50597d0
Howard Tseng <howardt12345@users.noreply.github.com>
2020-11-09 20:44:06 -0500

    Added MaterialDayPicker dependency

--- app/src/main/java/com/uottawa/servicenovigrad/branch/Branch.java
+++ app/src/main/java/com/uottawa/servicenovigrad/branch/Branch.java
@@ -1,8 +1,14 @@
 package com.uottawa.servicenovigrad.branch;
 
+import com.uottawa.servicenovigrad.service.Service;
+
+import java.util.List;
+
 public class Branch {
     private String name;
     private String description;
     private String address;
     private String phoneNumber;
+
+    private List<Service> services;
 }
