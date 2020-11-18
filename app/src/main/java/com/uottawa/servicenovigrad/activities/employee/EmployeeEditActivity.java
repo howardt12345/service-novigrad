46e05221b8ec4b77066af93066de18accf8d2ddc
Howard Tseng <howardt12345@users.noreply.github.com>
2020-11-17 22:36:28 -0500

    Update EmployeeEditActivity.java

--- app/src/main/java/com/uottawa/servicenovigrad/activities/employee/EmployeeEditActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/employee/EmployeeEditActivity.java
@@ -15,6 +15,7 @@ import com.uottawa.servicenovigrad.service.Service;
 public class EmployeeEditActivity extends AppCompatActivity {
 
     Branch branch;
+    private boolean newBranch = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +28,26 @@ public class EmployeeEditActivity extends AppCompatActivity {
             //Get the service from the data
             branch = (Branch) getIntent().getSerializableExtra("branch");
         } else {
+            //Change title to reflect current function
             TextView title = (TextView) findViewById(R.id.employee_edit_title);
             title.setText("Complete Branch Profile");
             title.setTextSize(24.0f);
 
+            //Hide cancel button
             ImageButton cancelButton = (ImageButton) findViewById(R.id.cancel_button_employee);
             cancelButton.setVisibility(View.GONE);
+
+            newBranch = true;
         }
     }
 
     @Override
     public void onBackPressed() {
         //Cancel the edit when back button is pressed
+        if(newBranch) {
+
+        } else {
+            
+        }
     }
 }
