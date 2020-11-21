c71ad5ca5ca7c3dca65d08804ebc26b4cbcbb6da
Howard Tseng <howardt12345@users.noreply.github.com>
2020-11-21 14:40:37 -0500

    Initialized service picker activity

---
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/service/ServicePickerActivity.java
@@ -0,0 +1,16 @@
+package com.uottawa.servicenovigrad.activities.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uottawa.servicenovigrad.R;
+
+public class ServicePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_picker);
+    }
+}
