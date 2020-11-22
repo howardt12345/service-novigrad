73f795bf3ceaa7449adbacf4b5f1081cbdea53a7
Howard Tseng <howardt12345@users.noreply.github.com>
2020-11-22 16:41:25 -0500

    Approving and rejecting services

--- app/src/main/java/com/uottawa/servicenovigrad/activities/branch/BranchServiceRequestsFragment.java
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/branch/BranchServiceRequestsFragment.java
@@ -29,7 +29,9 @@ import com.uottawa.servicenovigrad.service.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,6 +42,8 @@ public class BranchServiceRequestsFragment extends Fragment {
 
     private CollectionReference requestsReference = FirebaseFirestore.getInstance().collection("requests");
 
+    private Context context;
+    private LayoutInflater inflater;
 
     private static final String ARG_BRANCHID = "branchId";
 
@@ -77,6 +81,9 @@ public class BranchServiceRequestsFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_branch_service_requests, container, false);
 
+        context = rootView.getContext();
+        this.inflater = inflater;
+
         pendingRequestsList = rootView.findViewById(R.id.branch_service_requests_pending);
         approvedRequestsList = rootView.findViewById(R.id.branch_service_requests_approved);
         rejectedRequestsList = rootView.findViewById(R.id.branch_service_requests_rejected);
@@ -100,6 +107,12 @@ public class BranchServiceRequestsFragment extends Fragment {
                         //Get the information of the service
                         String id = doc.getId();
                         String customerId = doc.getString("customer");
+                        String serviceId = doc.getString("service");
+
+                        String customerName = doc.getString("customerName");
+                        String branchName = doc.getString("branchName");
+                        String serviceName = doc.getString("serviceName");
+
                         ArrayList<String> info = (ArrayList<String>) doc.get("info");
 
                         Timestamp scheduledTime = doc.getTimestamp("scheduledTime");
@@ -107,15 +120,15 @@ public class BranchServiceRequestsFragment extends Fragment {
                         boolean approved = doc.getBoolean("approved");
                         boolean responded = doc.getBoolean("responded");
 
-                        ServiceRequest request = new ServiceRequest(id, customerId, branchId, info, scheduledTime.toDate(), approved, responded);
+                        ServiceRequest request = new ServiceRequest(id, customerId, branchId, serviceId, customerName, branchName, serviceName, info, scheduledTime.toDate(), approved, responded);
 
                         serviceRequests.add(request);
                     }
                 }
 
-                setUpPendingRequestsList(serviceRequests, pendingRequestsList, rootView.getContext(), inflater);
-                setUpApprovedRequestsList(serviceRequests, approvedRequestsList, rootView.getContext(), inflater);
-                setUpRejectedRequestsList(serviceRequests, rejectedRequestsList, rootView.getContext(), inflater);
+                setUpPendingRequestsList(serviceRequests, pendingRequestsList, context, inflater);
+                setUpApprovedRequestsList(serviceRequests, approvedRequestsList, context, inflater);
+                setUpRejectedRequestsList(serviceRequests, rejectedRequestsList, context, inflater);
             }
         });
 
@@ -145,13 +158,25 @@ public class BranchServiceRequestsFragment extends Fragment {
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    requestInfo(requests.get(finalI));
+                    requestInfo(filteredRequests.get(finalI));
                 }
             });
 
             ImageButton approveButton = view.findViewById(R.id.approve_request);
+            approveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    approveRequest(filteredRequests.get(finalI));
+                }
+            });
 
             ImageButton rejectButton = view.findViewById(R.id.reject_request);
+            rejectButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rejectRequest(filteredRequests.get(finalI));
+                }
+            });
 
             ImageButton undoButton = view.findViewById(R.id.undo_reject_request);
             undoButton.setVisibility(View.GONE);
@@ -183,7 +208,7 @@ public class BranchServiceRequestsFragment extends Fragment {
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    requestInfo(requests.get(finalI));
+                    requestInfo(filteredRequests.get(finalI));
                 }
             });
 
@@ -223,7 +248,7 @@ public class BranchServiceRequestsFragment extends Fragment {
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    requestInfo(requests.get(finalI));
+                    requestInfo(filteredRequests.get(finalI));
                 }
             });
 
@@ -234,6 +259,12 @@ public class BranchServiceRequestsFragment extends Fragment {
             rejectButton.setVisibility(View.GONE);
 
             ImageButton undoButton = view.findViewById(R.id.undo_reject_request);
+            undoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    approveRequest(filteredRequests.get(finalI));
+                }
+            });
 
             //Add the list item to the list view
             listView.addView(view);
@@ -241,12 +272,11 @@ public class BranchServiceRequestsFragment extends Fragment {
     }
 
     private void requestInfo(ServiceRequest request) {
-
         //Create new AlertDialog
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
         alertDialogBuilder
-                .setTitle("") //Set the title of the dialog to the service name
-                .setMessage("") //Set the message of the dialog to the service info
+                .setTitle(request.getTitle()) //Set the title of the dialog to the service name
+                .setMessage(request.getRequestInfo()) //Set the message of the dialog to the service info
                 .setCancelable(true)
                 .setPositiveButton(
                         "CLOSE",
@@ -257,6 +287,90 @@ public class BranchServiceRequestsFragment extends Fragment {
                             }
                         }
                 );
+        //Show AlertDialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void approveRequest(final ServiceRequest request) {
+        //Create new AlertDialog
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder
+        .setTitle("Approve Request?") //Set the title of the dialog to the service name
+        .setMessage("Are you sure you want to approve this request? \nTHIS ACTION CANNOT BE UNDONE. \nThe customer will be notified immediately.") //Set the message of the dialog to the service info
+        .setCancelable(true)
+        .setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Create a map with the data to write to cloud firestore
+                        Map<String, Object> requestInfo = new HashMap<>();
+                        requestInfo.put("responded", true);
+                        requestInfo.put("approved", true);
+                        //Approve Request
+                        requestsReference.document(request.getId()).update(requestInfo);
+                        //Update lists
+                        setUpPendingRequestsList(serviceRequests, pendingRequestsList, context, inflater);
+                        setUpApprovedRequestsList(serviceRequests, approvedRequestsList, context, inflater);
+                        setUpRejectedRequestsList(serviceRequests, rejectedRequestsList, context, inflater);
+
+                        dialog.cancel();
+                    }
+                }
+        )
+        .setNegativeButton(
+                "NO",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Close the dialog without closing the activity
+                        dialog.cancel();
+                    }
+                }
+        );
+        //Show AlertDialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void rejectRequest(final ServiceRequest request) {
+        //Create new AlertDialog
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder
+        .setTitle("Request Request?") //Set the title of the dialog to the service name
+        .setMessage("Are you sure you want to reject this request? \nThe customer will be notified. \nThis can be undone until the date of the request.") //Set the message of the dialog to the service info
+        .setCancelable(true)
+        .setPositiveButton(
+            "OK",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Create a map with the data to write to cloud firestore
+                    Map<String, Object> requestInfo = new HashMap<>();
+                    requestInfo.put("responded", true);
+                    requestInfo.put("approved", false);
+                    //Reject Request
+                    requestsReference.document(request.getId()).update(requestInfo);;
+                    //Update lists
+                    setUpPendingRequestsList(serviceRequests, pendingRequestsList, context, inflater);
+                    setUpApprovedRequestsList(serviceRequests, approvedRequestsList, context, inflater);
+                    setUpRejectedRequestsList(serviceRequests, rejectedRequestsList, context, inflater);
+
+                    dialog.cancel();
+                }
+            }
+        )
+        .setNegativeButton(
+            "NO",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Close the dialog without closing the activity
+                    dialog.cancel();
+                }
+            }
+        );
         //Show AlertDialog
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
