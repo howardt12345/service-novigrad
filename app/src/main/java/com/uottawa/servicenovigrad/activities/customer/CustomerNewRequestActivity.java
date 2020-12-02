3c49c718f7ec61277933980587073e57dee2cfaa
Howard Tseng <howardt12345@users.noreply.github.com>
2020-12-02 15:47:14 -0500

    Select branch and service

--- app/src/main/java/com/uottawa/servicenovigrad/activities/customer/CustomerNewRequestActivity.java
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/customer/CustomerNewRequestActivity.java
@@ -9,13 +9,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.uottawa.servicenovigrad.R;
 import com.uottawa.servicenovigrad.activities.admin.AdminServicesEdit;
 import com.uottawa.servicenovigrad.activities.employee.EmployeeEditActivity;
 import com.uottawa.servicenovigrad.activities.employee.EmployeeMainActivity;
+import com.uottawa.servicenovigrad.activities.service.ServicePickerActivity;
 import com.uottawa.servicenovigrad.branch.Branch;
 import com.uottawa.servicenovigrad.service.Service;
+import com.uottawa.servicenovigrad.utils.Utils;
 
 public class CustomerNewRequestActivity extends AppCompatActivity {
 
@@ -23,6 +26,8 @@ public class CustomerNewRequestActivity extends AppCompatActivity {
 
     Branch branch;
     Service service;
+
+    Button branchButton, serviceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,24 @@ public class CustomerNewRequestActivity extends AppCompatActivity {
             Intent intent = new Intent(CustomerNewRequestActivity.this, CustomerSearchActivity.class);
             startActivityForResult(intent, GET_BRANCH);
         }
+
+        branchButton = findViewById(R.id.request_select_branch);
+        serviceButton = findViewById(R.id.request_select_service);
+    }
+
+    public void selectService(View view) {
+        if(branch != null) {
+            Intent intent = new Intent(CustomerNewRequestActivity.this, ServicePickerActivity.class);
+            intent.putExtra("branch", branch);
+            startActivityForResult(intent, GET_SERVICE);
+        } else {
+            Utils.showSnackbar("Select a branch first.", getCurrentFocus());
+        }
+    }
+
+    public void selectBranch(View view) {
+        Intent intent = new Intent(CustomerNewRequestActivity.this, CustomerSearchActivity.class);
+        startActivityForResult(intent, GET_BRANCH);
     }
 
 
@@ -43,14 +66,15 @@ public class CustomerNewRequestActivity extends AppCompatActivity {
         if(resultCode == RESULT_OK) {
             if(requestCode == GET_BRANCH) {
                 branch = (Branch) data.getSerializableExtra("branch");
-                Log.d("CustomerNewRequest", branch.getId());
+                branchButton.setText(branch.getName());
+                service = null;
+                serviceButton.setText("Select Service");
             } else if (requestCode == GET_SERVICE) {
                 service = (Service) data.getSerializableExtra("service");
+                serviceButton.setText(service.getName());
             }
         }
     }
-
-
 
     @Override
     public void onBackPressed() {
