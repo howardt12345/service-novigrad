a054694e9184ac4f3b21448908435b2e12863f31
Ethan Plant <plant.ethan@gmail.com>
2020-12-01 19:19:52 -0500

    Filter list of search results

--- app/src/main/java/com/uottawa/servicenovigrad/activities/customer/adapters/SearchResultListAdapter.java
+++ app/src/main/java/com/uottawa/servicenovigrad/activities/customer/adapters/SearchResultListAdapter.java
@@ -11,10 +11,14 @@ import com.uottawa.servicenovigrad.R;
 import com.uottawa.servicenovigrad.branch.Branch;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SearchResultListAdapter extends ArrayAdapter<Branch> {
+    ArrayList<Branch> allBranches;
+
     public SearchResultListAdapter(Context context, ArrayList<Branch> branches) {
         super(context, 0, branches);
+        allBranches = new ArrayList<>();
     }
 
     @Override
@@ -31,5 +35,26 @@ public class SearchResultListAdapter extends ArrayAdapter<Branch> {
         branchRating.setText("Rating: " + b.getRating());
 
         return convertView;
+    }
+
+    public void notifyDataSetChanged(Branch b) {
+        notifyDataSetChanged();
+        if (!allBranches.contains(b)) allBranches.add(b);
+    }
+
+    public void filter(String query) {
+        query = query.toLowerCase(Locale.getDefault());
+        clear();
+        if (query.length() == 0) addAll(allBranches);
+        else {
+            for (Branch b : allBranches) {
+                if (b.getName().toLowerCase(Locale.getDefault()).contains(query)
+                    || b.getAddress().toLowerCase(Locale.getDefault()).contains(query)
+                    || b.getName().toLowerCase(Locale.getDefault()).equals(query)) {
+                    add(b);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
